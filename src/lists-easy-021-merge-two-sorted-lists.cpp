ad2d406dd380eea428e2e96fda1613dd7007438f
Oliver Ocean <github@oliverocean.co>
2020-06-09 15:45:35 -0500

    revised documentation, code cleanup

--- src/lists-easy-021-merge-two-sorted-lists.cpp
+++ src/lists-easy-021-merge-two-sorted-lists.cpp
@@ -1,91 +1,149 @@
+/* *
+ * @author: Oliver Ocean <github@oliverocean.co>
+ * @project: LeetCode
+ * @title: Merge Two Sorted Lists
+ * @index: 021
+ * @difficulty: easy
+ * @topic: linked lists
+ * @reqs: https://leetcode.com/problems/merge-two-sorted-lists/
+ * @brief:
+ *     Merge two sorted linked lists and return it as a new sorted list. The new
+ *     list should be made by splicing together the nodes of the first two lists.
+ * @example:
+ *         Input: 1->2->4, 1->3->4
+ *         Output: 1->1->2->3->4->4
+ */
+
 #include <iostream>
 #include <vector>
+#include <climits>
 
-// Definition for singly-linked list.
-class ListNode {
+/* --- [ singly-linked list (given definition) ]--- */
+class ListNode
+{
 public:
     int val;
     ListNode* next;
     ListNode(int x) : val(x), next(nullptr) {}
 };
 
-// function prototypes
-void createList(std::vector<ListNode> &list);
-void printList(ListNode *l);
+/* ---[ function prototypes ]--- */
+void createList(std::vector<ListNode> &aList);
+void printList(ListNode *aList);
 ListNode* mergeTwoLists(ListNode*, ListNode*);
+ListNode* mergeTwoListsDebug(ListNode*, ListNode*);
 
+/* ---[ function test driver ]--- */
+int main()
+{
+    std::vector<ListNode> listOne{1, 2, 4, 6};
+    std::vector<ListNode> listTwo{1, 3, 4, 8};
 
-int main() {
+    createList(listOne);
+    createList(listTwo);
 
-    // useful test harness found on LC
-    std::vector<ListNode> l1{1, 2, 4, 6};
-    std::vector<ListNode> l2{1, 3, 4, 8};
-    createList(l1);
-    std::cout << "1: ";
-    printList(&l1[0]);
-    createList(l2);
-    std::cout << "2: ";
-    printList(&l2[0]);
-    mergeTwoLists(&l1[0], &l2[0]);
-    std::cout << "3: ";
-    printList(&l1[0]);
-    std::cout << "4: ";
-    printList(&l2[0]);
+    std::cout << "\n";
+    std::cout << "> List One: ";
+    printList(&listOne[0]);
+    std::cout << "> List Two: ";
+    printList(&listTwo[0]);
+
+    std::cout << "\n<< merging lists...\n\n";
+    mergeTwoLists(&listOne[0], &listTwo[0]);
+
+    std::cout << "> List One: ";
+    printList(&listOne[0]);
+    std::cout << "> List Two: ";
+    printList(&listTwo[0]);
 
     return 0;
 }
 
-// approach:
-// make a dummy node, use a 'tail' pointer to move back and forth
-// across the two lists, swap the pointers according to size,
-// repoint the list nodes to the next item (and 'seize' the evaluated nodes)
-// and keep the tail pointed to the last node of the 'new' list
-// after one list runs out, add the remaining list to the end and done
+/* ---[ final approach ]--- */
+ListNode* mergeTwoLists(ListNode* listOne, ListNode* listTwo)
+{
+    auto head = new ListNode(INT_MIN);
+    ListNode* tail = head;
 
-ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+    while (listOne && listTwo)
+    {
+        if (listOne->val < listTwo->val)
+        {
+            tail->next = listOne;
+            listOne = listOne->next;
+        }
+        else
+        {
+            tail->next = listTwo;
+            listTwo = listTwo->next;
+        }
+        tail = tail->next;
+    }
+    return head->next;
+}
 
-    // head to be returned at end (using dummy node)
+
+/* ---[ final approach with notes ]--- */
+ListNode* mergeTwoListsDebug(ListNode* listOne, ListNode* listTwo)
+{
+    /* Notes: make a dummy node, use a 'tail' pointer to move back and forth
+     * across the two lists, swap the pointers according to size,
+     * repoint the list nodes to the next item (and 'seize' the evaluated nodes)
+     * and keep the tail pointed to the last node of the 'new' list
+     * after one list runs out, add the remaining list to the end and done*/
+
+    // return head at end (using dummy node)
     auto head = new ListNode(INT_MIN);
 
     // pointer to traverse the lists
     ListNode* tail = head;
 
-    // note, the first round points head/dummy node to the smallest item
-    while (l1 && l2) {
-        if (l1->val < l2->val) {
-            tail->next = l1;
-            l1 = l1->next;
-        } else {
-            tail->next = l2;
-            l2 = l2->next;
+    // note: first iteration points head/dummy node to the smallest item
+    while (listOne && listTwo)
+    {
+        if (listOne->val < listTwo->val)
+        {
+            tail->next = listOne;
+            listOne = listOne->next;
+        }
+        else
+        {
+            tail->next = listTwo;
+            listTwo = listTwo->next;
         }
         tail = tail->next;
     }
 
-    // Add the (larger) item(s) from the remaining list
-    // if l1 points to nullptr, 'tail->next' = l2, otherwise l1 has items left over to add
-    // can also do:
-    // tail->next = l1 ? l1 : l2
-    tail->next = (l1 == nullptr) ? l2 : l1;
+    /* Add the (larger) item(s) from the remaining list if listOne points to nullptr,
+     * 'tail->next' = listTwo, otherwise listOne has items left over to add
+     * can also do:
+     * tail->next = listOne ? listOne : listTwo
+     * tail->next = (listOne == nullptr) ? listTwo : listOne; */
 
     return head->next; // skips dummy node - is there a memory leak concern?
 }
 
-
-// useful test harness found on LC
-void createList(std::vector<ListNode>& list) {
-    ListNode *nnode = nullptr;
-    for (auto node = list.rbegin(); node != list.rend(); ++node) {
-        node->next = nnode;
-        nnode = &(*node);
+/* ---[ modified test function found on LC ]--- */
+void createList(std::vector<ListNode>& aList)
+{
+    ListNode *nextNode = nullptr;
+    for (auto node = aList.rbegin(); node != aList.rend(); ++node)
+    {
+        node->next = nextNode;
+        nextNode = &(*node);
     }
 }
 
-void printList(ListNode* l) {
-    std::cout << "• ";
-    while (l) {
-        std::cout << l->val << " • ";
-        l = l->next;
+/* ---[ test helper function ]--- */
+void printList(ListNode* aList)
+{
+    std::cout << "[ ";
+    while (aList)
+    {
+        std::cout << aList->val << ", ";
+        aList = aList->next;
     }
-    std::cout << "\n";
+    std::cout << "]\n";
 }
+
+// EOF
