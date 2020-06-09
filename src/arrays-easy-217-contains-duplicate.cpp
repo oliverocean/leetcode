93fe609fac20cff2770855eea62f791f5dc0de09
Oliver Ocean <github@oliverocean.co>
2020-06-09 10:34:56 -0500

    added and revised documentation

--- src/arrays-easy-217-contains-duplicate.cpp
+++ src/arrays-easy-217-contains-duplicate.cpp
@@ -1,17 +1,45 @@
+/* *
+ * @author: Oliver Ocean <github@oliverocean.co>
+ * @project: LeetCode
+ * @title: Contains Duplicate
+ * @index: 217
+ * @difficulty: easy
+ * @topic: array, set, hashtable
+ * @reqs: https://leetcode.com/problems/contains-duplicate/
+ * @brief:
+ *     Given an array of integers, find if the array contains any duplicates.
+ *     Your function should return true if any value appears at least twice
+ *     in the array, and it should return false if every element is distinct.
+ * @example:
+ *     Example 1:
+ *         Input: [1,2,3,1]
+ *         Output: true
+ *     Example 2:
+ *         Input: [1,2,3,4]
+ *         Output: false
+ *     Example 3:
+ *         Input: [1,1,1,3,3,4,3,2,4,2]
+ *         Output: true
+ */
+
 #include <iostream>
 #include <vector>
 #include <unordered_set>
+#include <algorithm>
 
-// function prototype
+/* ---[ function prototype ]--- */
 bool containsDuplicate(std::vector<int>&);
+bool containsDuplicateAltOne(std::vector<int>&);
+bool containsDuplicateAltTwo(std::vector<int>&);
+bool containsDuplicateAltThree(std::vector<int>&);
+bool containsDuplicateAltFour(std::vector<int>&);
 
-
-int main() {
-
+/* ---[ function test driver ]--- */
+int main()
+{
     //std::vector<int>input {1, 2, 3, 1}; // true
     //std::vector<int>input {1, 2, 3, 4}; // false
     std::vector<int>input {1, 1, 1, 3, 3, 4, 3, 2, 4, 2}; // true
-
     std::cout << "\n---[ input ]---\n";
     std::cout << "vector: [ ";
     for (const auto& i: input) {
@@ -21,72 +49,80 @@ int main() {
 
     //std::cout << "\n---[ debug ]---\n";
     int output = containsDuplicate(input);
-
     std::cout << "\n---[ output ]---\n";
     std::cout << "result: [ " << output << " ]\n";
 
     return 0;
 }
 
-// best (second) approach, eval returns early if dupe found
-// (recall that search/insert unordered set/map is O(1) on average)
-// runtime: 36ms @ 75.83%, memory: 14.9mb @79.12%
-bool containsDuplicate(std::vector<int>& nums) {
-
+/* ---[ revised approach, fast and easy to understand ]--- */
+/* runtime: 36ms @ 76%, memory: 14.9mb @ 79% */
+bool containsDuplicate(std::vector<int>& nums)
+{
     std::unordered_set<int> aSet;
 
-    for (auto n : nums) {
-        if (aSet.find(n) != aSet.end()) {
+    for (auto n : nums)
+    {
+        if (aSet.find(n) != aSet.end())
+        {
             return true; // duplicate found in set, return early
-        } else {
+        }
+        else
+        {
             aSet.insert(n);
         }
     }
     return false; // no duplicate found
 }
 
-// another approach is sort, then look for consecutive items
-// NOTE: using adjacent_find() instead of for loop, which is quite interesting
-// sort is n(log n) but this modifies the input so not optimal
-//bool containsDuplicate(std::vector<int>& nums) {
-//    std::sort(nums.begin(), nums.end());
-//    return nums.end() != std::adjacent_find(nums.begin(), nums.end());
-//}
+/* ---[ pre-sort approach ]--- */
+/* Using an adjacent_find() instead of for loop */
+bool containsDuplicateAltOne(std::vector<int>& nums)
+{
+    std::sort(nums.begin(), nums.end()); // n(log n) but modifying input likely suboptimal
+    return nums.end() != std::adjacent_find(nums.begin(), nums.end());
+}
 
-// initial attempt
-// less efficient as it assesses entire vector
-// without shortcutting if a duplicate is found
-// runtime: 44ms @ 46.24%, memory: 16.3mb @ 73.63%
-//bool containsDuplicate(std::vector<int>& nums) {
-//
-//    std::unordered_set<int> aSet;
-//
-//    for (auto n : nums) {
-//        aSet.insert(n);
-//    }
-//
-//    return nums.size() > aSet.size();
-//}
 
-// here is a shorter version of my initial attempt
-// initializes and evaluates unordered_set (vs vector) in the return statement
-//bool containsDuplicate(std::vector<int>& nums) {
-//    //return std::unordered_set<int>{nums.begin(), nums.end()}.size() != nums.size();
-//    return nums.size() != std::unordered_set<int>(nums.begin(), nums.end()).size();
-//}
+/* ---[ revised brute force ]--- */
+/* shorter version, initialize/eval set (vs vector) in return statement */
+bool containsDuplicateAltTwo(std::vector<int>& nums)
+{
+    return nums.size() != std::unordered_set<int>(nums.begin(), nums.end()).size();
+    //return std::unordered_set<int>{nums.begin(), nums.end()}.size() != nums.size();
+}
 
-// another approach using the .second() interator, which returns
-// false if an element is already in a set
-// I think the booleans aren't as easy to understand in this
-// situation (function returns 'true' if there is a duplicate) so not optimal
-//bool containsDuplicate(std::vector<int>& nums) {
-//
-//    std::unordered_set<int> aSet;
-//
-//    for (auto n : nums) {
-//        if (aSet.insert(n).second) { // false if element already in set
-//            return true;
-//        }
-//    }
-//    return false;
-//}
+/* ---[ brute force ]--- */
+/* runtime: 44ms @ 46%, memory: 16.3mb @ 74% */
+/* Notes: less efficient as it assesses entire vector without shortcutting if a duplicate is found */
+bool containsDuplicateAltThree(std::vector<int>& nums)
+{
+    std::unordered_set<int> aSet;
+
+    for (auto n : nums)
+    {
+        aSet.insert(n);
+    }
+
+    return nums.size() > aSet.size();
+}
+
+/* ---[ iterator approach ]--- */
+/* using .second() iterator, which returns false if an element is already in a set.
+ * I think booleans aren't as easy to understand in this specific situation
+ * (function returns 'true' if there is a duplicate) so less optimal solution */
+bool containsDuplicateAltFour(std::vector<int>& nums)
+{
+    std::unordered_set<int> aSet;
+
+    for (auto n : nums)
+    {
+        if (aSet.insert(n).second) // false if element already in set
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+// EOF
