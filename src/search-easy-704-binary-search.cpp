e03c510d06fa7a954a0cd2a69981d4ae1abbc29a
Oliver Ocean <github@oliverocean.co>
2020-06-09 16:47:44 -0500

    revised documentation

--- src/search-easy-704-binary-search.cpp
+++ src/search-easy-704-binary-search.cpp
@@ -1,10 +1,39 @@
+/* *
+ * @author: Oliver Ocean <github@oliverocean.co>
+ * @project: LeetCode
+ * @title: Binary Search
+ * @index: 704
+ * @difficulty: easy
+ * @topic: search, binary search
+ * @reqs: https://leetcode.com/problems/binary-search/
+ * @brief:
+ *     Given a sorted (in ascending order) integer array 'nums' of 'n' elements
+ *     and a 'target' value, write a function to search 'target' in 'nums'.
+ *     If 'target' exists, return it's index, otherwise return -1.
+ * @example:
+ *     Example 1:
+ *         Input: nums = [ -1, 0, 3, 5, 9, 12 ], target = 9
+ *         Output: 4
+ *         Explain: 9 exists in 'nums' and it's index = 4
+ *     Example 2:
+ *         Input: nums = [ -1, 0, 3, 5, 9, 12 ], target = 2
+ *         Output: -1
+ *         Explain: 2 does not exist in 'nums', so return -1
+ * @notes:
+ *     > Assume all elements in 'nums' are unique
+ *     > 'n' will be in the range [ 1, 10000 ]
+ *     > The value of each element in 'nums' will be in the range [ -9999, 9999 ]
+ */
+
 #include <iostream>
 #include <vector>
 
+/* ---[ function prototypes ]--- */
 int search(std::vector<int>&, int);
 
-int main() {
-
+/* ---[ function test driver ]--- */
+int main()
+{
     std::vector<int> items{1, 0, 3, 5, 6, 7, 8, 44, 66, 9, 12};
     int target{6};
 
@@ -14,27 +43,33 @@ int main() {
     return 0;
 }
 
-// binary search
-int search(std::vector<int>& items, int target) {
-
-    if (items.empty()) return -1; // corner case
+/* ---[ final approach ]--- */
+int search(std::vector<int>& items, int target)
+{
+    if (items.empty()) { return -1; }
 
     int low{};
     int high{static_cast<int>(items.size() - 1)};
     int guess{};
 
-    while (low <= high) {
+    while (low <= high)
+    {
         guess = low + (high - low) / 2;
 
-        if (items[guess] == target) {
+        if (items[guess] == target)
+        {
             return guess;
-        } else if (items[guess] < target) {
+        }
+        else if (items[guess] < target)
+        {
             low = guess + 1;
-        } else {
+        }
+        else
+        {
             high = guess - 1;
         }
     }
-
-    // per requirements, return -1 if nothing is found
     return -1;
 }
+
+// EOF
