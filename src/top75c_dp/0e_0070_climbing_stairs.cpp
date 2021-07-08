e0ba0dc1de13662cf96c0d5e79d331ee508a54fb
Oliver Ocean <github@oliverocean.co>
2021-07-08 17:34:23 -0500

    Revised solution

--- src/top75c_dp/0e_0070_climbing_stairs.cpp
+++ src/top75c_dp/0e_0070_climbing_stairs.cpp
@@ -4,6 +4,19 @@
 
 using namespace std;
 
+// standard dp
+int climb_stairs_(int n) {
+
+    vector<int>sum(n + 1, 1);
+
+    for (int i = 2; i <= n; i++) {
+        sum[i] = sum[i - 1] + sum[i - 2];
+    }
+
+    return sum[n];
+}
+
+// fibonacci sum
 int climb_stairs(int n) {
 
     int sum = 1;
@@ -15,18 +28,6 @@ int climb_stairs(int n) {
     }
 
     return sum;
-}
-
-// standard dp solution
-int climb_stairs_(int n) {
-
-    vector<int>sum(n + 1, 1);
-
-    for (int i = 2; i <= n; i++) {
-        sum[i] = sum[i - 1] + sum[i - 2];
-    }
-
-    return sum[n];
 }
 
 //--[ test driver ]--- */
